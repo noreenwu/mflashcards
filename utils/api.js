c25ded7662a47d707fb3148b3d5d5123ad88e6d9
Noreen Wu <noreenwu@gmail.com>
2019-10-09 20:46:58 -0400

    build: changes to app.json and to the starter decks for demo purposes

--- utils/api.js
+++ utils/api.js
@@ -5,25 +5,50 @@ const DECKS_STORAGE_KEY = 'Flashcards:decks';
 
 // these are starter decks, so the app loads with some data. 
 let decks = {
-  React: {
-    title: 'React',
+  'Latin Roots and Prefixes': {
+    title: 'Latin Roots and Prefixes',
     questions: [
       {
-        question: 'What is React?',
-        answer: 'A library for managing user interfaces'
+        question: 'mit, miss',
+        answer: 'send'
       },
       {
-        question: 'Where do you make Ajax requests in React?',
-        answer: 'The componentDidMount lifecycle event'
+        question: 'nounce, nunce',
+        answer: 'call'
+      },
+      {
+        question: 'port',
+        answer: 'carry'
       }
     ]
   },
-  JavaScript: {
-    title: 'JavaScript',
+  'Science: Evolution': {
+    title: 'Science: Evolution',
     questions: [
       {
-        question: 'What is a closure?',
-        answer: 'The combination of a function and the lexical environment within which that function was declared.'
+        question: 'What is a fossil?',
+        answer: 'Traces or imprints of living things, such as animals, plants, bacteria and fungi that are preserved in rock'
+      },
+      {
+        question: 'What is geologic timescale?',
+        answer: 'A calendar used to outline the history of life on Earth'
+      },
+      {
+        question: 'What are plate tectonics?',
+        answer: 'Wilson\'s theory of how huge pieces of crust move around the globe'
+      }
+    ]
+  },
+  Osmosis: {
+    title: 'Osmosis',
+    questions: [
+      {
+        question: 'pinocytosis',
+        answer: 'the ingestion of liquid into a cell by the budding of small vesicles from the cell membrane'
+      },
+      {
+        question: 'phagocytosis',
+        answer: 'A type of endocytosis in which a cell engulfs large particles or whole cells'
       }
     ]
   }
